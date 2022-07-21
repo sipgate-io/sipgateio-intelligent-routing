8551a90652c030ae2e1b0b0c84c83c6c7fa24954
Phil Pick <pick@sipgate.de>
2022-07-21 10:26:53 +0200

    :sparkles: add functionality to redirect a call
    Co-authored-by: Luca Schmitz <schmitz@sipgate.de>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,43 @@
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
+import { WebhookResponse, createWebhookModule } from 'sipgateio';
 
 dotenv.config();
 
-const msg = 'Hello, Typescript!';
-console.log(msg);
+const port = 8080;
+
+if (!process.env.SIPGATE_WEBHOOK_SERVER_ADDRESS) {
+  console.error(
+    'ERROR: You need to set a server address for the followup webhook events!\n',
+  );
+  process.exit();
+}
+
+const serverAddress = process.env.SIPGATE_WEBHOOK_SERVER_ADDRESS;
+const hostname = process.env.HOSTNAME || 'localhost';
+const redirectNumber = process.env.REDIRECT_PHONENUMBER;
+
+const webhookModule = createWebhookModule();
+webhookModule
+  .createServer({
+    port,
+    serverAddress,
+    hostname,
+  })
+  .then((webhookServer) => {
+    console.log(
+      `Server running at ${serverAddress}\n` +
+        'Please set this URL for incoming calls at https://console.sipgate.com/webhooks/urls\n' +
+        "ProTip: To see how to do that automatically, check out the example at 'examples/settings/settings_set_url_incoming.ts'\n" +
+        'Ready for calls 📞',
+    );
+
+    webhookServer.onNewCall((newCallEvent) => {
+      console.log(`New call from ${newCallEvent.from} to ${newCallEvent.to}`);
+
+      console.log('Redirecting...');
+      return WebhookResponse.redirectCall({
+        anonymous: true,
+        numbers: [],
+      });
+    });
+  });
