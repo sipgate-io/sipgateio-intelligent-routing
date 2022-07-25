a365dadc3bdcb4a5612ce99422e9e51012294a60
Phil Pick <pick@sipgate.de>
2022-07-25 15:32:13 +0200

    :sparkles: read and write database
    Co-authored-by: Nils Reck <reck@sipgate.de>
    Co-authored-by: Rene Lehmann <rlehmann@sipgate.de>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import * as dotenv from 'dotenv';
+import {
+  createConnection,
+  DataSource,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { WebhookResponse, createWebhookModule } from 'sipgateio';
 import getRandomIntInRange from './util';
+// import AppDataSource from "./dataSource.js";
 
 dotenv.config();
 
@@ -24,6 +32,37 @@ const serverAddress: string = process.env.SIPGATE_WEBHOOK_SERVER_ADDRESS;
 const hostname: string = process.env.HOSTNAME || 'localhost';
 const centralServiceNumber: string = process.env.CENTRAL_SERVICE_PHONE;
 const serviceTeamNumbers: string[] = process.env.SERVICE_PHONES.split(',');
+
+@Entity()
+class CallHistory {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  customerPhone: string;
+
+  @Column()
+  servicePhone: string;
+
+  @Column()
+  acceptedCalls: number;
+}
+
+const AppDataSource: DataSource = new DataSource({
+  type: 'mysql',
+  host: hostname,
+  username: 'root',
+  password: 'root',
+  port: 3306,
+  database: 'io-labs-telephone-status-request',
+  entities: [CallHistory],
+  synchronize: true,
+});
+
+AppDataSource.initialize().then(() => {
+  console.log('initialized.');
+  AppDataSource.synchronize();
+});
 
 const webhookModule = createWebhookModule();
 webhookModule
@@ -52,5 +91,43 @@ webhookModule
         anonymous: true,
         numbers: [redirectnumber],
       });
+    });
+
+    webhookServer.onHangUp(async (newHangupEvent) => {
+      console.log(
+        `New hangup from ${newHangupEvent.from} to ${newHangupEvent.to} (answering: ${newHangupEvent.answeringNumber})`,
+      );
+      await AppDataSource.createQueryBuilder()
+        .insert()
+        .into(CallHistory)
+        .values([
+          {
+            customerPhone: '+4945555555555550',
+            servicePhone: '+4945555555555559',
+            acceptedCalls: 8,
+          },
+          {
+            customerPhone: '+4945555555555550',
+            servicePhone: '+4945555555555558',
+            acceptedCalls: 3,
+          },
+          {
+            customerPhone: '+4945555555555550',
+            servicePhone: '+4945555555555557',
+            acceptedCalls: 2,
+          },
+          {
+            customerPhone: '+4945555555555550',
+            servicePhone: '+4945555555555556',
+            acceptedCalls: 9,
+          },
+        ])
+        .execute();
+
+      const results = await AppDataSource.createQueryBuilder()
+        .select('*')
+        .from(CallHistory, 'call_history')
+        .execute();
+      console.log(results);
     });
   });
